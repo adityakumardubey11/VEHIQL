72fb970fee9578c59c6c0da6ab1f70f288423790
aditya dubey <adityaxmom112005@gmail.com>
2025-04-30 03:50:09 +0530

    prep for deployment

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -0,0 +1,11 @@
+import { SignIn } from "@clerk/nextjs"
+import React from 'react'
+
+const SignUpPage = () => {
+  return (
+   <SignUp />
+  )
+}
+
+export default SignUp 
+
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverComponentsHmrCache: false, //default to true
+    },
+    
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "peoutgqwghxgoimcmypz.supabase.co"
+            },
+        ],
+    },
+    async headers(){
+        return [
+           {
+            source:"/embeded",
+            headers:[
+                {
+                key:"Content-Security-Policy",
+                value: "frame-src 'self' https://vehiqle.created.app;",
+                },
+            ],
+           },
+        ]
+    }
+};
 
 export default nextConfig;
